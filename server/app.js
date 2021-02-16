72cca3de2237b675e36010b6fddaa125d452557b
azkarmoulana <azkar.moulana@gmail.com>
2021-02-16 23:32:22 +0530

    added new mongo connection

--- server/app.js
+++ server/app.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema")
+const mongoose = require("mongoose");
 
 const app = express();
 
 const PORT = 4000;
+
+const DB_URL = "mongodb+srv://user:1234@cluster0.bdjdf.mongodb.net/GRAPHQL-PLAYLIST?retryWrites=true&w=majority";
+
+// mongoose.connect(DB_URL);
+// mongoose.connection.once("open", () => {
+//     console.log('connected to database')
+// });
+const m = mongoose.connect(DB_URL, {useNewUrlParser: true});
+m.then(res => {
+    console.log('connected successfully ', res)
+}).catch(e => {
+    console.log('error connecting db ', e)
+})
 
 app.use('/graphql', graphqlHTTP({
     schema,
