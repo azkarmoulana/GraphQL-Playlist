900b691886dbc86c750330c45b41f0817928ac08
azkarmoulana <azkar.moulana@gmail.com>
2021-02-16 16:51:31 +0530

    created app server

---
+++ server/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema")
+
+const app = express();
+
+const PORT = 4000;
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}));
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+});
