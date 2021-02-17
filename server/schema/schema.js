c8ebeef65a68ef6e6052349249e1a3939ff92f23
azkarmoulana <azkar.moulana@gmail.com>
2021-02-17 16:13:36 +0530

    modified resolve functions to get data from DB

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -22,6 +22,7 @@ const BookType = new GraphQLObjectType({
       type: AuthorType,
       resolve(parent, args) {
         // return _.find(authors, { id: parent.authorId });
+        return Author.findById(parent.authorId);
       },
     },
   }),
@@ -37,6 +38,7 @@ const AuthorType = new GraphQLObjectType({
       type: new GraphQLList(BookType),
       resolve(parent, args) {
         // return _.filter(books, { authorId: parent.id });
+        return Book.find({ authorId: parent.id });
       },
     },
   }),
@@ -51,6 +53,7 @@ const RootQuery = new GraphQLObjectType({
       resolve(parent, args) {
         // code to get data from DB / other sources
         // return _.find(books, { id: args.id });
+        return Book.findById(args.id);
       },
     },
 
@@ -59,6 +62,7 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         // return _.find(authors, { id: args.id });
+        return Author.findById(args.id);
       },
     },
 
@@ -66,6 +70,7 @@ const RootQuery = new GraphQLObjectType({
       type: new GraphQLList(BookType),
       resolve(parent, args) {
         // return books;
+        return Book.find({});
       },
     },
 
@@ -73,6 +78,7 @@ const RootQuery = new GraphQLObjectType({
       type: new GraphQLList(AuthorType),
       resolve(parent, args) {
         // return authors;
+        return Author.find({});
       },
     },
   },
@@ -93,6 +99,24 @@ const Mutation = new GraphQLObjectType({
           age: args.age,
         });
         return author.save();
+      },
+    },
+
+    addBook: {
+      type: BookType,
+      args: {
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString },
+        authorId: { type: GraphQLID },
+      },
+      resolve(parent, args) {
+        let book = new Book({
+          name: args.name,
+          genre: args.genre,
+          authorId: args.authorId,
+        });
+
+        return book.save();
       },
     },
   },
