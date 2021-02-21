e07b0c17edb40843cc9a23385a3da3d703645c89
azkarmoulana <azkar.moulana@gmail.com>
2021-02-21 21:47:47 +0530

    enabled CORS

--- server/app.js
+++ server/app.js
@@ -1,30 +1,38 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
-const schema = require("./schema/schema")
+const schema = require("./schema/schema");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
 const PORT = 4000;
 
-const DB_URL = "mongodb+srv://user:1234@cluster0.bdjdf.mongodb.net/GRAPHQL-PLAYLIST?retryWrites=true&w=majority";
+// enabling CORS
+app.use(cors());
+
+const DB_URL =
+  "mongodb+srv://user:1234@cluster0.bdjdf.mongodb.net/GRAPHQL-PLAYLIST?retryWrites=true&w=majority";
 
 // mongoose.connect(DB_URL);
 // mongoose.connection.once("open", () => {
 //     console.log('connected to database')
 // });
-const m = mongoose.connect(DB_URL, {useNewUrlParser: true});
-m.then(res => {
-    console.log('connected successfully ', res)
-}).catch(e => {
-    console.log('error connecting db ', e)
-})
+const m = mongoose.connect(DB_URL, { useNewUrlParser: true });
+m.then((res) => {
+  console.log("connected successfully ", res);
+}).catch((e) => {
+  console.log("error connecting db ", e);
+});
 
-app.use('/graphql', graphqlHTTP({
+app.use(
+  "/graphql",
+  graphqlHTTP({
     schema,
-    graphiql: true
-}));
+    graphiql: true,
+  })
+);
 
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`)
+  console.log(`Listening on port ${PORT}`);
 });
